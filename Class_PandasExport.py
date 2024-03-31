d8df668bdb240e5295b84b958670b8bb162619b8
Stefan <stefdrp@gmail.com>
2024-03-31 12:46:08 +0200

    update 31 March

--- Class_PandasExport.py
+++ Class_PandasExport.py
@@ -2,20 +2,39 @@ import os
 import pandas as pd
 
 class PandasExporter:
+    """
+    A class to export Pandas DataFrame to tab-separated text files.
+    """
     def __init__(self, dataframe):
+        """
+        Initialize PandasExporter with a DataFrame object.
+        
+        Parameters:
+        dataframe (DataFrame): The DataFrame to export.
+        """
+        if not isinstance(dataframe, pd.DataFrame):
+            raise ValueError("Input must be a Pandas DataFrame")
         self.dataframe = dataframe
 
     def export_to_txt(self, file_name=None):
+        """
+        Export DataFrame to a tab-separated text file.
+        
+        Parameters:
+        file_name (str): The name of the output text file. If not provided, defaults to "output.txt".
+        """
         if file_name is None:
             file_name = "output.txt"
         file_path = os.path.join(os.getcwd(), file_name)
         
-        # Dataframe veranderen in een TSV opzet
+        # Convert DataFrame to TSV-formatted string
         df_string = self.dataframe.to_csv(index=False, sep='\t')
-
-        # String wegschrijven naar txt bestand
+        
+        # Write string to text file
         with open(file_path, 'w') as file:
             file.write(df_string)
+
+
 
 
 
--- Class_lees_bedrijven_update.py
+++ Class_lees_bedrijven_update.py
@@ -14,21 +14,6 @@ class BedrijvenDataReader:
             column_names = ["Code", "Naam", "Straat", "Huisnr", "Postcd", "Plaats", "Xwaarde", "Ywaarde", "Maxuitst", "Beruitst", "Boete", "Controle", "Freq", "Ctpers"]
             column_types = {"Code": str, "Naam": str, "Straat": str, "Huisnr": str, "Postcd": str, "Plaats": str, "Xwaarde": int, "Ywaarde": int, "Maxuitst": int, "Beruitst": float, "Boete": float, "Controle": str, "Freq": float, "Ctpers": str}
             bedrijven = pd.read_csv(self.update_file2, sep='\t', names=column_names, dtype=column_types, header = 0)
-        elif os.path.exists(self.update_file): 
-            bedrijven = pd.read_fwf(self.update_file, names=["Code", 
-                                                        "Naam", 
-                                                        "Straat", 
-                                                        "Huisnr", 
-                                                        "Postcd", 
-                                                        "Plaats", 
-                                                        "Xwaarde", 
-                                                        "Ywaarde", 
-                                                        "Maxuitst", 
-                                                        "Beruitst", 
-                                                        "Boete", 
-                                                        "Controle", 
-                                                        "Freq", 
-                                                        "Ctpers"])
         else: 
             colspecs = [(0, 4), (4, 24), (24, 54), (54, 59), (59, 65), (65, 85), (85, 87), (87, 89), (89, 99), (99, 109), (109, 117), 
                         (117, 118), (118, 120), (120, 140)]
